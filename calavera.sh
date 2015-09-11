d51d5d0c40ae7a303880c3b97dcc7322886b78d3
Kévin Dunglas <dunglas@gmail.com>
2015-09-11 19:22:08 +0200

    Cleanup calavera.sh

--- calavera.sh
+++ calavera.sh
@@ -1,47 +1,5 @@
 #!/bin/sh
 
-# Equivalent to Python's os.path.relpath
-# http://stackoverflow.com/a/12498485/1352334
-function relpath
-{
-    source=$1
-    target=$2
-
-    common_part=$source # for now
-    result="" # for now
-
-    while [[ "${target#$common_part}" == "${target}" ]]; do
-        # no match, means that candidate common part is not correct
-        # go up one level (reduce common part)
-        common_part="$(dirname $common_part)"
-        # and record that we went back, with correct / handling
-        if [[ -z $result ]]; then
-            result=".."
-        else
-            result="../$result"
-        fi
-    done
-
-    if [[ $common_part == "/" ]]; then
-        # special case for root (no common path)
-        result="$result/"
-    fi
-
-    # since we now have identified the common part,
-    # compute the non-common part
-    forward_part="${target#$common_part}"
-
-    # and now stick all parts together
-    if [[ -n $result ]] && [[ -n $forward_part ]]; then
-        result="$result$forward_part"
-    elif [[ -n $forward_part ]]; then
-        # extra slash removal
-        result="${forward_part:1}"
-    fi
-
-    return $result
-}
-
 # Cleanup previous build
 
 rm -Rf gowrkspc/ app/doc/
