0914e4615f020dc56220988072b16e738fad6373
Rodrigue Villetard <rodrigue@les-tilleuls.coop>
2016-10-13 10:41:18 +0200

    fix: html entities when navigating with javascript enabled fix: images loading

--- src/redux/modules/page.js
+++ src/redux/modules/page.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { getPage } from 'helpers/DataClient';
+import path from 'path';
 const cheerio = require('cheerio');
 
 
@@ -40,7 +41,26 @@ export function requestPage(pageName) {
 }
 
 export function receivePage(pageName, jsonldDoc, data) {
-  const wrap = cheerio.load('' + data.text);
+  let baseHtml = data.text;
+
+  // Doing some stuff that require browser only API (chiefly DOM things)
+  if ( !__SERVER__) {
+    // Highlight code blocks
+    const doc = (new DOMParser).parseFromString(baseHtml, 'text/html');
+    const codeBlocks = doc.querySelectorAll('code[class*="language-"]');
+    let iterator;
+    for (iterator = 0; iterator < codeBlocks.length; iterator++) {
+      Prism.highlightElement(codeBlocks[iterator]);
+    }
+
+    // Add anchors
+    // Does not work with cheerio. We need to instantiate a DOMParser
+    // (new AnchorJS()).add(wrap('h2, h3, h4, h5, h6'));
+
+    baseHtml = doc.body.innerHTML;
+  }
+
+  const wrap = cheerio.load('' + baseHtml);// data.text);
 
   wrap('a').each((index, anchor) => {
     const $anchor = wrap(anchor);
@@ -59,31 +79,12 @@ export function receivePage(pageName, jsonldDoc, data) {
     const src = $image.attr('src');
         // Convert relative URLs
     if (!/^(?:[a-z]+:)?\/\//i.test(src)) {
-      $image.attr('src', '/data/' + src);
+      const separator = (/\/$/.test(pageName)) ? '.' : '..';
+      const picUrl = path.join('/data/', pageName, separator, src);
+      $image.attr('src', picUrl);// '/data/' + src);
     }
   });
 
-  // Highlight code blocks
-  if ( !__SERVER__) {
-    wrap('code[class*="language-"]').each((index, code) => {
-      const element = cheerio(code);
-      const initialHtml = element.html();
-
-      const language = element.attr('class')
-                              .split(/\s+/)
-                              .find(item => item.match(/^language-/))
-                              .match(/-(.+)$/)[1]
-                              ;
-
-      if (__PRISMJS_LANGUAGES__.indexOf(language) >= 0) {
-        element.html(Prism.highlight(initialHtml, Prism.languages[language]));
-      }
-    });
-
-    // Add anchors
-    // Does not work with cheerio. We need to instantiate a DOMParser
-    // (new AnchorJS()).add(wrap('h2, h3, h4, h5, h6'));
-  }
 
   return {
     type: RECEIVE_PAGE,
