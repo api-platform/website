16e3bd262e3a998429b8739eeb40a5810ad6fa16
Kévin Dunglas <dunglas@gmail.com>
2016-10-16 13:23:38 +0200

    Add Google Analytics support

--- src/config.js
+++ src/config.js
@@ -16,21 +16,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'Calavera',
+    description: 'A (static) Single Page Application generator using Markdown files.',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'Calavera: %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'A (static) Single Page Application generator using Markdown files.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'Calavera'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'Calavera'},
+        {property: 'og:description', content: 'A (static) Single Page Application generator using Markdown files.'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@dunglas'},
+        {property: 'og:creator', content: '@dunglas'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
