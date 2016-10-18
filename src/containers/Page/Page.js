4d52a2f593f6f1915073f704b8693ab66fb935b0
Kévin Dunglas <dunglas@gmail.com>
2016-10-18 08:55:59 +0200

    Make img responsives

--- src/containers/Page/Page.js
+++ src/containers/Page/Page.js
@@ -11,79 +11,78 @@ if (!__SERVER__) {
 // TODO: use https://github.com/reactjs/react-magic/blob/master/README-htmltojsx.md
 
 class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.captureClicks = this.captureClicks.bind(this);
+  }
 
-    constructor(props) {
-      super(props);
-      this.captureClicks = this.captureClicks.bind(this);
-    }
+  componentWillMount() {
+    this.addAnchors();
+  }
 
-    componentWillMount() {
-      this.addAnchors();
-    }
+  componentDidUpdate() {
+    this.addAnchors();
+  }
 
-    componentDidUpdate() {
-      this.addAnchors();
-    }
-
-    getHtml() {
-      if (!this.props.currentPage || this.props.currentPage.isFetching ) {
-        return {
-          __html: ''
-        };
-      }
-
+  getHtml() {
+    if (!this.props.currentPage || this.props.currentPage.isFetching ) {
       return {
-        __html: this.props.currentPage.data.text,
+        __html: ''
       };
     }
 
-    addAnchors() {
-      if (!__SERVER__) {
-        (new AnchorJS()).add();
-      }
+    return {
+      __html: this.props.currentPage.data.text,
+    };
+  }
+
+  addAnchors() {
+    if (!__SERVER__) {
+      (new AnchorJS()).add();
+    }
+  }
+
+  /**
+   * Manually capture clicks on links to prevent full page refresh
+   */
+  captureClicks(event) {
+    const target = event.target;
+
+    // handle "a" tag only
+    if (target.tagName.toLowerCase() !== 'a') {
+      return;
     }
 
-    /**
-     * Manually capture clicks on links to prevent full page refresh
-     */
-    captureClicks(event) {
-      const target = event.target;
+    const url = target.getAttribute('href');
 
-      // handle "a" tag only
-      if (target.tagName.toLowerCase() !== 'a') {
-        return;
-      }
-
-      const url = target.getAttribute('href');
-
-      // handle relative urls only and page anchors
-      if (/^(?:[a-z]+:)?\/\//i.test(url) || /^#/.test(url)) {
-        return;
-      }
-
-      // prevent full page refresh
-      event.preventDefault();
-
-      // handle the case where last fragment is wether a directory or a file basename
-      const basePath = this.props.selectedPage.substring(0, this.props.selectedPage.lastIndexOf('/') + 1);
-
-      // finally use our spa's router
-      this.props.navigateTo(path.join('/', basePath, url));
+    // handle relative urls only and page anchors
+    if (/^(?:[a-z]+:)?\/\//i.test(url) || /^#/.test(url)) {
+      return;
     }
 
-    render() {
-      if (this.props.currentPage.notFound) {
-        return (<div className="error">Document not found.</div>);
-      }
+    // prevent full page refresh
+    event.preventDefault();
 
-      return (
-        // The HTML is properly sanitized by the Go JSON-LD generator.
-        <section>
-          <article className="page" onClick={this.captureClicks} dangerouslySetInnerHTML={this.getHtml()} />
-          <EditOnGithub pagepath={this.props.currentDocumentPath} />
-        </section>
-      );
+    // handle the case where last fragment is wether a directory or a file basename
+    const basePath = this.props.selectedPage.substring(0, this.props.selectedPage.lastIndexOf('/') + 1);
+
+    // finally use our spa's router
+    this.props.navigateTo(path.join('/', basePath, url));
+  }
+
+  render() {
+    if (this.props.currentPage.notFound) {
+      return (<div className="error">Document not found.</div>);
     }
+
+    return (
+      // The HTML is properly sanitized by the Go JSON-LD generator.
+      <section>
+        <article className="page" onClick={this.captureClicks} dangerouslySetInnerHTML={this.getHtml()} />
+        <EditOnGithub pagepath={this.props.currentDocumentPath} />
+      </section>
+    );
+  }
 }
 
 Page.propTypes = {
