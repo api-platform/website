bfe91d6a74b272695277674c0327dd0f31981d20
christian <tuanphpvn@gmail.com>
2017-10-16 13:59:25 +0700

    Update description, keywords for seo
    SEO benefits

--- src/config.js
+++ src/config.js
@@ -21,14 +21,15 @@ module.exports = Object.assign({
     head: {
       titleTemplate: 'API Platform: %s',
       meta: [
-        {name: 'description', content: 'A PHP framework dedicated to hypermedia API creation.'},
+        {name: 'description', content: 'A PHP framework dedicated to hypermedia API creation, auto generate api from entities.'},
+        {name: 'keywords', content: 'PHP framework, Api First Framework, auto generate api from entities, auto generate admin.'},
         {charset: 'utf-8'},
         {property: 'og:site_name', content: 'API Platform'},
         {property: 'og:image', content: '/logo.png'},
         {property: 'og:image:type', content: 'image/png'},
         {property: 'og:locale', content: 'en_US'},
         {property: 'og:title', content: 'API Platform'},
-        {property: 'og:description', content: 'A PHP framework dedicated to hypermedia API creation.'},
+        {property: 'og:description', content: 'A PHP framework dedicated to hypermedia API creation, auto generate api from entities.'},
         {property: 'og:card', content: 'summary'},
         {property: 'og:site', content: '@ApiPlatform'},
         {property: 'og:creator', content: '@dunglas'},
