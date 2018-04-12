d5564e947e99fa54b5e0fcf53a4208d1688612cd
jordanaubert <jordan@les-tilleuls.coop>
2018-04-12 17:28:48 +0200

    feat nav : improve nav generation

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -52,6 +52,10 @@ export const pageQuery = graphql`
             anchors {
               id
               title
+              anchors {
+                id
+                title
+              }
             }
           }
         }
