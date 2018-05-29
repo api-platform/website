965af56848c5b5258ce8cd2036f6f7652dd69808
Maxime Lafarie <maxime.lafarie@biig.fr>
2018-05-29 16:19:51 +0200

    Add link to companies list and fix mispellings

--- src/data/logos.js
+++ src/data/logos.js
@@ -20,109 +20,136 @@ export const otherData = [
   {
     name: '24 Sèvres',
     logo: '24-sevres',
+    link: 'https://www.24sevres.com/',
   },
   {
     name: 'Adeo',
     logo: 'adeo',
+    link: 'https://www.adeo.com/',
   },
   {
     name: 'Alstom',
     logo: 'alstom',
+    link: 'http://www.alstom.com/',
   },
   {
-    name: 'Being Sports',
-    logo: 'being-sports',
+    name: 'Bein Sports',
+    logo: 'bein-sports',
+    link: 'http://www.beinsports.com/',
   },
   {
     name: 'BiiG',
     logo: 'biig',
+    link: 'https://www.biig.fr/',
   },
   {
     name: 'BNP',
     logo: 'bnp',
+    link: 'https://mabanque.bnpparibas/',
   },
   {
     name: 'Decathlon',
     logo: 'decathlon',
+    link: 'https://www.decathlon.fr/',
   },
   {
     name: 'eVaali.fi',
     logo: 'evaali',
+    link: 'https://www.evaali.fi/',
   },
   {
     name: 'Exaqtworld',
     logo: 'exaqtworld',
+    link: 'http://www.exaqtworld.com/',
   },
   {
     name: 'The Fork',
     logo: 'fork',
+    link: 'https://www.thefork.com/',
   },
   {
     name: 'France 24',
     logo: 'france-24',
+    link: 'http://www.france24.com/',
   },
   {
     name: 'General Electric',
     logo: 'general-electric',
+    link: 'https://www.ge.com/',
   },
   {
     name: 'HoHey.fr',
     logo: 'hohey',
+    link: 'http://hohey.fr/',
   },
   {
     name: 'M6',
     logo: 'm6',
+    link: 'http://www.groupem6.fr/',
   },
   {
     name: 'MEL',
     logo: 'mel',
+    link: 'http://www.lillemetropole.fr/',
   },
   {
     name: 'Microdon',
     logo: 'microdon',
+    link: 'https://www.microdon.org/',
   },
   {
     name: 'Parti de gauche',
     logo: 'parti-de-gauche',
+    link: 'https://www.lepartidegauche.fr/',
   },
   {
     name: 'Quotatis',
     logo: 'quotatis',
+    link: 'https://www.quotatis.fr/',
   },
   {
     name: 'Racepack',
     logo: 'racepack',
+    link: 'https://www.racepack.com/',
   },
   {
     name: 'Renault',
     logo: 'renault',
+    link: 'https://www.renault.fr/',
   },
   {
     name: 'Ville de Roubaix',
     logo: 'roubaix',
+    link: 'http://www.ville-roubaix.fr/',
   },
   {
     name: 'Saveur Bière',
     logo: 'saveur-biere',
+    link: 'https://www.saveur-biere.com/',
   },
   {
     name: 'Sensio Labs',
     logo: 'sensio-labs',
+    link: 'https://sensiolabs.com/',
   },
   {
     name: 'Smile',
     logo: 'smile',
+    link: 'https://www.smile.eu/',
   },
   {
     name: 'Les-Tilleuls.coop',
     logo: 'les-tilleuls',
+    link: 'https://les-tilleuls.coop/',
   },
   {
     name: 'TMM',
     logo: 'tmm',
+    link: 'http://www.tmm-groupe.com/',
   },
   {
     name: 'YouSign',
     logo: 'yousign',
+    link: 'https://yousign.com/',
   },
 ];
