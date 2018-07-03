5f9e728c7f46abf83a2504f87216d46b6b3f1be6
Mathieu Dewet <mathieu.dewet@gmail.com>
2018-07-03 16:23:55 +0200

    refactor fileprocessor with helpers

---
+++ src/lib/fileContentHelper.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { markdown } = require('markdown');
+const path = require('path');
+
+const fileContentHelper = {
+  getFileContent(filePath, id) {
+    const fileUrl = path.resolve(__dirname, `../pages/docs/${filePath}/${id}.md`);
+    const content = fs.readFileSync(fileUrl).toString()
+      .replace(/([^`])(`)([^`])/gm, '$1$3')
+      .replace(/```(|yaml|xml|php|json|javascript|sh)[^`]*```/gm, '');
+
+    return markdown.toHTML(content);
+  },
+};
+
+module.exports = fileContentHelper;
