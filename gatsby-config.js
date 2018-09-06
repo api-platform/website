acc444dcbe113e295c2cc24f375a01f84fe65206
Fabious <fabious@free.fr>
2018-09-06 09:17:25 +0200

    use short syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,9 +19,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-relative-images`,
-          },
+          `gatsby-remark-relative-images`,
           {
             resolve: `gatsby-remark-images`,
             options: {
