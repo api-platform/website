cf1595da822cee6b7224671c918d87c59605f553
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-04-11 20:30:27 +0200

    run linter

--- src/components/docs/SwitchVersion.js
+++ src/components/docs/SwitchVersion.js
@@ -1,33 +1,30 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import {navigate} from 'gatsby'
-import {versions} from '../../../constants';
+import { navigate } from 'gatsby';
+import { versions } from '../../../constants';
 
-const doChange = (e, currentVersion) => {
-    const targetVersion = isNaN(e.target.value) ? e.target.value : `v${e.target.value}`;
-    let path = location.pathname.replace(currentVersion, targetVersion);
-    navigate(path);
+const doChange = (e, location, currentVersion) => {
+  const targetVersion = !Number.isNaN(e.target.value) ? e.target.value : `v${e.target.value}`;
+  const path = location.pathname.replace(currentVersion, targetVersion);
+  navigate(path);
 };
 
-const SwitchVersion = ({ currentVersion }) => {
+const SwitchVersion = ({ location, currentVersion }) => (
+<select onChange={e => doChange(e, location, currentVersion)} value={currentVersion}>
+  {versions.map(version => {
+    const formattedVersion = Number.isNaN(Number(version)) ? version : `v${version}`;
     return (
-        <select onChange={(e) => doChange(e, currentVersion)} value={currentVersion}>
-            {versions.map(version => {
-                version = isNaN(version) ? version : `v${version}`;
-                return (
-                    <option key={version} value={version}>{version}</option>
-                )
-            })}
-        </select>
-    )
-};
-
-SwitchVersion.propTypes = {
-    currentVersion: PropTypes.string.isRequired,
-};
-
-SwitchVersion.defaultProps = {
-    currentVersion: 'stable',
-};
+      <option key={formattedVersion} value={formattedVersion}>
+        {formattedVersion}
+      </option>
+    );
+  })}
+</select>
+);
 
 export default SwitchVersion;
+
+SwitchVersion.propTypes = {
+  location: PropTypes.object.isRequired,
+  currentVersion: PropTypes.string.isRequired,
+};
