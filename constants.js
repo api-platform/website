009b9e2feecc32ee3ab72c316c1278b73047e7a9
Alan Poulain <contact@alanpoulain.eu>
2019-05-02 17:50:32 +0200

    Fixes for check-outline

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
   docPagesDirectory: `${__dirname}/src/pages/docs/`,
+  outlinePath: `${__dirname}/config/outline.yaml`,
   current: 'stable',
   versions: ['master', '2.3', '2.2', '2.1'],
 });
