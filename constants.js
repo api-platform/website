7d06ae70b6a3f035f3f1497bfff30359602d46fe
Kévin Dunglas <dunglas@gmail.com>
2019-09-30 17:11:07 +0200

    Bump versions

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
   docPagesDirectory: `${__dirname}/src/pages/docs/`,
   current: 'current',
-  versions: ['master', '2.3', '2.2', '2.1'],
-  currentVersion: '2.4',
-  masterVersion: '2.5',
+  versions: ['master', '2.4', '2.3', '2.2', '2.1'],
+  currentVersion: '2.5',
+  masterVersion: '2.6',
   siteUrl: 'https://api-platform.com',
 });
