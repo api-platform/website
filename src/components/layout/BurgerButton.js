0df6f9e674d4985c86e75b88623d98edab89927d
Justine <justine.zahiri@les-tilleuls.coop>
2019-10-14 16:20:04 +0200

    moved script tag inside body tag in html.js file

--- src/components/layout/BurgerButton.js
+++ src/components/layout/BurgerButton.js
@@ -1,12 +1,8 @@
-import PropTypes from "prop-types";
-import React from "react";
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const BurgerButton = ({ status, onClick, className }) => (
-  <button
-    className={`btn-burger ${status} ${className}`}
-    onClick={onClick}
-    aria-label="open the menu"
-  >
+  <button className={`btn-burger ${status} ${className}`} onClick={onClick} aria-label="open the menu">
     <span className="btn-burger__back">
       <span className="btn-burger__line" />
     </span>
@@ -16,13 +12,13 @@ const BurgerButton = ({ status, onClick, className }) => (
 BurgerButton.propTypes = {
   status: PropTypes.string,
   onClick: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 BurgerButton.defaultProps = {
-  status: "burger",
+  status: 'burger',
   onClick: null,
-  className: ""
+  className: '',
 };
 
 export default BurgerButton;
--- src/html.js
+++ src/html.js
@@ -16,10 +16,9 @@ export default class HTML extends React.Component {
           {this.props.preBodyComponents}
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
-        </body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.twttr = (function(d, s, id) {
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.twttr = (function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0],
                   t = window.twttr || {};
                 if (d.getElementById(id)) return t;
@@ -35,8 +34,9 @@ export default class HTML extends React.Component {
 
                 return t;
               }(document, "script", "twitter-wjs"));`,
-          }}
-        />
+            }}
+          />
+        </body>
       </html>
     );
   }
