3b0544d0d4999fed9ba3a94483ef820200487567
Justine <justine.zahiri@les-tilleuls.coop>
2019-10-14 14:40:55 +0200

    added aria-label for burger menu

--- src/components/layout/BurgerButton.js
+++ src/components/layout/BurgerButton.js
@@ -1,8 +1,12 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
 const BurgerButton = ({ status, onClick, className }) => (
-  <button className={`btn-burger ${status} ${className}`} onClick={onClick}>
+  <button
+    className={`btn-burger ${status} ${className}`}
+    onClick={onClick}
+    aria-label="open the menu"
+  >
     <div className="btn-burger__back">
       <div className="btn-burger__line" />
     </div>
@@ -12,13 +16,13 @@ const BurgerButton = ({ status, onClick, className }) => (
 BurgerButton.propTypes = {
   status: PropTypes.string,
   onClick: PropTypes.func,
-  className: PropTypes.string,
+  className: PropTypes.string
 };
 
 BurgerButton.defaultProps = {
-  status: 'burger',
+  status: "burger",
   onClick: null,
-  className: '',
+  className: ""
 };
 
 export default BurgerButton;
