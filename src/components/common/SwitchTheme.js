924fd865cb9136a575fad26989ad2fb429cc95f5
ginifizz <laury@les-tilleuls.coop>
2020-06-19 13:42:13 +0200

    fix: @alanpoulain pr review

--- src/components/common/SwitchTheme.js
+++ src/components/common/SwitchTheme.js
@@ -3,11 +3,7 @@ import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 
 const Moon = () => (
   <svg className="moon" viewBox="0 0 32 32">
-    <path
-      d="M24.633,22.184c-8.188,0-14.82-6.637-14.82-14.82c0-2.695,0.773-5.188,2.031-7.363
-				C5.02,1.968,0,8.188,0,15.645C0,24.676,7.32,32,16.352,32c7.457,0,13.68-5.023,15.648-11.844
-				C29.82,21.41,27.328,22.184,24.633,22.184z"
-    />
+    <path d="M24.633,22.184c-8.188,0-14.82-6.637-14.82-14.82c0-2.695,0.773-5.188,2.031-7.363 C5.02,1.968,0,8.188,0,15.645C0,24.676,7.32,32,16.352,32c7.457,0,13.68-5.023,15.648-11.844 C29.82,21.41,27.328,22.184,24.633,22.184z" />
   </svg>
 );
 
@@ -33,6 +29,7 @@ const Sun = () => (
   </svg>
 );
 
+/* eslint-disable jsx-a11y/label-has-associated-control */
 const SwitchTheme = () => (
   <ThemeToggler>
     {({ theme, toggleTheme }) => (
@@ -50,7 +47,6 @@ const SwitchTheme = () => (
           className="switch-theme__label"
           title={`Switch to ${'dark' === theme ? 'light' : 'dark'} mode`}
         >
-          {' '}
           <span className="switch-theme__icon">
             <Moon />
             <Sun />
