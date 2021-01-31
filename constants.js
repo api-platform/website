52ade404d0907d68a668c4b861c2009302b45769
Alan Poulain <contact@alanpoulain.eu>
2021-01-31 15:06:12 +0100

    fix: typo in constants

--- constants.js
+++ constants.js
@@ -3,6 +3,6 @@ module.exports = Object.freeze({
   current: 'current',
   versions: ['main', '2.5', '2.4', '2.3', '2.2', '2.1'],
   currentVersion: '2.6',
-  mainnVersion: '2.7',
+  mainVersion: '2.7',
   siteUrl: 'https://api-platform.com',
 });
