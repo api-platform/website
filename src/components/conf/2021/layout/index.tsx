6d4c4d095c0614f03f2c8992270183d231278b1a
ginifizz <laury@les-tilleuls.coop>
2021-03-07 09:34:18 +0100

    feat: add footer

--- src/components/conf/2021/layout/index.tsx
+++ src/components/conf/2021/layout/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import Logo from '../images/logo.svg';
+import Tilleuls from '../images/tilleuls.svg';
 import Nav from './Nav';
 
 dayjs.extend(localizedFormat);
@@ -28,6 +30,21 @@ const Layout: React.ComponentType = ({ children }) => {
     <div className="conf full scrollable" ref={container}>
       <Nav withScroll={hasScroll} />
       {children}
+      <div className="conf__footer">
+        <div className="container">
+          <img src={Logo} className="footer__logo" alt="Api Platform Conference" />
+          <span className="footer__by">an event by</span>
+          <a className="footer__tilleuls" href="https://les-tilleuls.coop/en" target="_blank" rel="noreferrer noopener">
+            <img src={Tilleuls} alt="Les-Tilleuls.coop" />
+          </a>
+          <span className="body2 footer__copyright">
+            Copyright © 2021{' '}
+            <a href="https://les-tilleuls.coop/en" target="_blank" rel="noreferrer noopener">
+              Les-tilleuls.coop
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
