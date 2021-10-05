e1ff3b1724c4d1ba9d669cedd3c34dadd3027cf9
Laury S <ginifizz@users.noreply.github.com>
2021-10-05 16:40:29 +0200

    fix: nav issue and meta (#261)

--- src/components/con/layout/index.tsx
+++ src/components/con/layout/index.tsx
@@ -6,8 +6,11 @@ import MobileNav from '@components/con/layout/MobileNav';
 import Nav from '@con/layout/Nav';
 import Footer from '@con/layout/Footer';
 import PreloadFonts from '@con/layout/Fonts';
+import Helmet from 'react-helmet';
+import { DESCRIPTION, TITLE, OG_IMAGE, URL } from '@con/data/meta';
 import { Navigation } from '@con/types';
 import nav from '@con/data/nav';
+import helmetConfig from '../../../helmetConfig';
 
 dayjs.extend(localizedFormat);
 
@@ -24,9 +27,38 @@ interface LayoutProps {
 }
 
 const Layout: React.ComponentType<LayoutProps> = ({ children, logoAlwaysVisible = false, edition }) => {
+  const websiteData = {
+    '@context': 'https://schema.org',
+    '@type': 'Website',
+    name: 'API Platform Conference',
+    url: 'https://api-platform.com/con/',
+  };
+
   return (
     <ConfContext.Provider value={{ nav }}>
       <PreloadFonts />
+      <Helmet {...helmetConfig.head}>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:url" content={URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={OG_IMAGE} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@coopTilleuls" />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={OG_IMAGE} />
+
+        <script type="application/ld+json">{JSON.stringify(websiteData)}</script>
+        <script defer src="https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js" />
+        <style type="text/css">{`
+          body, html {
+            background-color: #001226;
+          }
+    `}</style>
+      </Helmet>
       <div className="conf conf__layout">
         <div className="conf__background" />
         <Nav logoAlwaysVisible={logoAlwaysVisible} edition={edition} />
