8c4ae042301e9fab8a000f3be0c5b2cdf5a86e97
Loïc Vernet <vernet.loic@gmail.com>
2022-02-06 21:20:31 +0100

    fix: contact links

--- src/pages/community.js
+++ src/pages/community.js
@@ -119,7 +119,7 @@ const CommunityPage = ({ location, data }) => {
                 </p>
                 <br />
                 <p>
-                  <a href="https://les-tilleuls.coop/en/contact" target="_blank" rel="noopener noreferrer">
+                  <a href="https://les-tilleuls.coop/en#contact" target="_blank" rel="noopener noreferrer">
                     Contact us
                   </a>{' '}
                   for more information.
@@ -187,7 +187,7 @@ const CommunityPage = ({ location, data }) => {
                 </p>{' '}
                 <br />
                 <p>
-                  <a href="https://les-tilleuls.coop/en/contact" target="_blank" rel="noopener noreferrer">
+                  <a href="https://les-tilleuls.coop/en#contact" target="_blank" rel="noopener noreferrer">
                     Contact Les-Tilleuls.coop
                   </a>{' '}
                   to organize a tailored training course.
