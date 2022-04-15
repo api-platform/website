f23177744f197874a80dacbb80a86e7f81a42ddc
ginifizz <laury@les-tilleuls.coop>
2022-04-15 17:44:00 +0200

    fix: 2022 meta

--- src/con/data/2022/meta.ts
+++ src/con/data/2022/meta.ts
@@ -1,5 +1,5 @@
-export const TITLE = 'API Platform Conference 2021';
-export const DESCRIPTION = 'The first international conference dedicated to API Platform and its ecosystem';
+export const TITLE = 'API Platform Conference 2022';
+export const DESCRIPTION = 'The international conference dedicated to API Platform and its ecosystem';
 export const OG_IMAGE = `${process.env.GATSBY_ROOT_URL}/conf-facebook.png`;
 export const URL = 'https://api-platform.com/con/2022/';
 const meta = {
