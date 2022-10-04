51e1bd593a01158f2242b0bb8d3342c6c807b43d
natacha-h <46324298+natacha-h@users.noreply.github.com>
2022-10-04 10:58:26 +0200

    feat: upgrade DocSearch to v3 (#327)

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,87 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import scriptLoader from 'react-async-script-loader';
-// import { currentVersion } from '../../../constants';
+import { DocSearch } from '@docsearch/react';
+import { currentVersion } from '../../../constants';
+import '@docsearch/css';
 
-class Search extends React.Component {
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const { isScriptLoaded } = this.props;
-    if (nextProps.isScriptLoaded && !isScriptLoaded) {
-      if (nextProps.isScriptLoadSucceed) {
-        this.initDocSearch();
-      }
-    }
-  }
-
-  componentDidMount() {
-    const { isScriptLoaded, isScriptLoadSucceed } = this.props;
-
-    if (isScriptLoaded && isScriptLoadSucceed) {
-      this.initDocSearch();
-    }
-  }
-
-  /* eslint-disable no-undef */
-  initDocSearch() {
-    if (docsearch) {
-      docsearch({
-        apiKey: process.env.GATSBY_DOCSEARCH_API_KEY,
-        indexName: process.env.GATSBY_DOCSEARCH_INDEX_NAME,
-        inputSelector: this.searchInput,
-        debug: false,
-        // algoliaOptions: { facetFilters: [`version:v${currentVersion}`] },
-      });
-    }
-  }
-  /* eslint-enable no-undef */
-
-  onSearchClick = () => {
-    this.searchInput.focus();
-  };
-
-  render() {
-    const { className, onFocus, onBlur } = this.props;
-
-    return (
-      <div className={classNames('search', className)}>
-        <button
-          className="icon-search search__icon"
-          type="button"
-          onClick={this.onSearchClick}
-          title="Search docs"
-          aria-label="Search docs"
-        />
-        <form>
-          <input
-            ref={(input) => {
-              this.searchInput = input;
-            }}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            className="search__input"
-            type="search"
-            placeholder="SEARCH..."
-          />
-        </form>
-      </div>
-    );
-  }
-}
+const Search = ({ className }) => (
+  <div className={classNames('search', className)}>
+    <DocSearch
+      appId={process.env.GATSBY_DOCSEARCH_APP_ID}
+      apiKey={process.env.GATSBY_DOCSEARCH_API_KEY}
+      indexName={process.env.GATSBY_DOCSEARCH_INDEX_NAME}
+      searchParameters={{
+        facetFilters: [`version:v${currentVersion}`],
+      }}
+      placeholder="Search..."
+    />
+  </div>
+);
 
 Search.propTypes = {
   className: PropTypes.string,
-  isScriptLoaded: PropTypes.bool.isRequired,
-  isScriptLoadSucceed: PropTypes.bool.isRequired,
-  onFocus: PropTypes.func,
-  onBlur: PropTypes.func,
 };
 
 Search.defaultProps = {
   className: '',
-  onFocus: () => {},
-  onBlur: () => {},
 };
 
-export default scriptLoader('https://cdn.jsdelivr.net/docsearch.js/2.5/docsearch.min.js')(Search);
+export default Search;
