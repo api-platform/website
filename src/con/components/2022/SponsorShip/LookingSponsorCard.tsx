aed105e60bcb8c379d5477b3467850c95d4b3f37
ginifizz <laury@les-tilleuls.coop>
2022-11-10 11:15:52 +0100

    fix: typo on sponsor card

--- src/con/components/2022/SponsorShip/LookingSponsorCard.tsx
+++ src/con/components/2022/SponsorShip/LookingSponsorCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LookingSponsorCard: React.ComponentType = () => (
   <div className="conf__sponsorcard">
     <h3 className="sponsorcard__title p-30 flex flex-col items-center justify-center">
-      <span className="overline">We&apos; looking for</span>
+      <span className="overline">We&apos;re looking for</span>
       <strong className="h5 lined lined-white">sponsors!</strong>
     </h3>
     <div className="dotted-corner conf__bg-white flex-1 px-40 py-20">
