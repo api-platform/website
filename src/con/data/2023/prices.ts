a1abdb91b345de5c0e1f9617c701844fed3fe84e
Laury S <ginifizz@users.noreply.github.com>
2023-04-26 19:03:25 +0200

    feat: update early bird date (#352)

--- src/con/data/2023/prices.ts
+++ src/con/data/2023/prices.ts
@@ -9,16 +9,16 @@ const prices: Price[] = [
       {
         title: '2 days on-site event <br>(Early bird)</br>',
         price: 119,
-        limitDate: '2023-05-14',
+        limitDate: '2023-04-24',
       },
       {
         title: '2 days on-site event',
         price: 159,
-        startDate: '2023-05-15',
+        startDate: '2023-04-25',
         limitDate: '2023-07-31',
       },
       {
-        title: '2 days on-site event',
+        title: '2 days on-site event <br>(Late bird)</br>',
         price: 209,
         startDate: '2023-08-01',
       },
