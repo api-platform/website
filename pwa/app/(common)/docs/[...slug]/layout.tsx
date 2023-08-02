6bba60a59b57813b23a6ebac873d9f39d9183e1f
soyuka <soyuka@users.noreply.github.com>
2023-08-02 11:03:04 +0200

    feat: reference and guides

--- pwa/app/(common)/docs/[...slug]/layout.tsx
+++ pwa/app/(common)/docs/[...slug]/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { loadV2DocumentationNav } from "api/doc";
 import { versions, current } from "consts";
 import DocLayout from "../components/DocLayout";
+import { getAllDocLinks } from "api/doc/guides";
 
 async function Layout({
   params: { slug },
@@ -12,10 +13,33 @@ async function Layout({
   };
   children: React.ReactNode;
 }) {
-  const version = versions.includes(slug[0].substring(1))
-    ? slug[0].substring(1)
-    : current;
-  const nav = await loadV2DocumentationNav(version);
+  let version = current;
+
+  if (slug && slug.length) {
+    const slugVersion = slug[0].substring(1);
+    if (versions.includes(slugVersion)) {
+      version = slugVersion
+    }
+  }
+
+  const v2Nav = await loadV2DocumentationNav(version);
+  const guideLinks = await getAllDocLinks("guides", "guide");
+
+  const nav = [
+    ...v2Nav.slice(0, 2),
+    {
+      title: 'Core API Reference',
+      basePath: '/docs/reference',
+      link: '/docs/references'
+    },
+    {
+      title: 'Core Guides',
+      basePath: '/docs/guide',
+      link: '/docs/guides',
+      links: guideLinks,
+    },
+    ...v2Nav.slice(2)
+  ];
 
   return <DocLayout nav={nav}>{children}</DocLayout>;
 }
