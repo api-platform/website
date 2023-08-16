fc4e07bfed1bb5f3d16ee85b5ab23a3569c8319d
Romain <romain.herault@les-tilleuls.coop>
2023-08-16 16:46:54 +0200

    feat: improve script

--- pwa/get-core-docs.sh
+++ pwa/get-core-docs.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 git clone https://github.com/api-platform/core /api-platform/core
-cd /api-platform/core
-composer install --no-dev
-composer require webonyx/graphql-php
 
 PDG_AUTOLOAD=/api-platform/core/vendor/autoload.php
 versions=("3.1" "main")
 for version in "${versions[@]}"
 do
+	cd /api-platform/core
 	git checkout $version
-	cp pdg.config.yaml /api-platform/core/docs
-	cd /api-platform/core/docs && /api-platform/tools/pdg guides
-	cd /api-platform/core/docs && /api-platform/tools/pdg references
+	composer install
+	composer require webonyx/graphql-php
+	cp /pdg.config.yaml /api-platform/core/docs
+	mkdir -p /data/docs/{guides,reference}/$version
+	cd /api-platform/core/docs && /api-platform/tools/pdg guides /data/docs/guides/$version
+	cd /api-platform/core/docs && /api-platform/tools/pdg references ../src /data/docs/reference/$version
 done
