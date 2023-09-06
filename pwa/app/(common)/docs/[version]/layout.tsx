e3fa2c4e0dea93f920c079b3879b71771b1e9ced
ginifizz <laury@les-tilleuls.coop>
2023-09-06 20:29:04 +0200

    fix: guides links

--- pwa/app/(common)/docs/[version]/layout.tsx
+++ pwa/app/(common)/docs/[version]/layout.tsx
@@ -3,6 +3,7 @@ import { loadV2DocumentationNav } from "api/doc";
 import { versions, current, refVersions } from "consts";
 import DocLayout from "../components/DocLayout";
 import { getAllDocLinks } from "api/doc/guides";
+import { NavPartProps } from "components/docs/DocMenu";
 
 export async function generateStaticParams() {
   return versions.map((v) => ({ version: `v${v}` }));
@@ -20,26 +21,24 @@ async function Layout({
     ? version.substring(1)
     : current;
 
-  const v2Nav = await loadV2DocumentationNav(v);
-
-  const guideLinks = await getAllDocLinks(`guides/${v}`, "guide", ".mdx", v);
-  const nav = refVersions.includes(v)
-    ? [
-        ...v2Nav.slice(0, 2),
-        {
-          title: "Core API Reference",
-          basePath: "/docs/reference",
-          link:
-            v === current ? "/docs/references" : `/docs/${version}/references`,
-        },
-        {
-          title: "Core Guides",
-          basePath: "/docs/guide",
-          links: guideLinks,
-        },
-        ...v2Nav.slice(2),
-      ]
-    : v2Nav;
+  let nav: NavPartProps[] = await loadV2DocumentationNav(v);
+  if (refVersions.includes(v)) {
+    const guideLinks = await getAllDocLinks(`guides/${v}`, "guide", ".mdx", v);
+    nav = [
+      ...nav.slice(0, 2),
+      {
+        title: "Core API Reference",
+        basePath: "/docs/reference",
+        link: `/docs/${version}/references`,
+      },
+      {
+        title: "Core Guides",
+        basePath: "/docs/guide",
+        links: guideLinks,
+      },
+      ...nav.slice(2),
+    ];
+  }
 
   return <DocLayout nav={nav}>{children}</DocLayout>;
 }
