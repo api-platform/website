ecf01244c66377bac501218873fa8f826f6f292c
ginifizz <laury@les-tilleuls.coop>
2023-09-11 15:02:58 +0200

    fix: schedule last updates

--- pwa/data/con/2023/days.ts
+++ pwa/data/con/2023/days.ts
@@ -1,23 +1,5 @@
 const days = [
   {
-    date: "2023-09-19",
-    title: {
-      fr: "Workshop 1",
-      en: "Workshop 1",
-    },
-    tracks: null,
-    single: true,
-  },
-  {
-    date: "2023-09-20",
-    title: {
-      fr: "Workshop 2",
-      en: "Workshop 2",
-    },
-    tracks: null,
-    single: true,
-  },
-  {
     date: "2023-09-21",
     title: {
       fr: "Conference jour 1",
