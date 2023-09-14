1de4803ebfafeea6c428b8e658936f67f5166efb
Romain <romain.herault@les-tilleuls.coop>
2023-09-14 09:59:37 +0200

    feat: adding fetch changelog to core docs script

--- pwa/tools/get-core-docs.sh
+++ pwa/tools/get-core-docs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This script fetches api-platform/core to retrieve guides and references
 root=$(pwd)
+readarray -t allVersions < docs-versions.txt
+versions=("3.1" "main")
+
 rm -rf core.temp
 git clone -b main --single-branch --depth=1 https://github.com/api-platform/core core.temp
 cd core.temp
 
 export PDG_AUTOLOAD=$root/core.temp/vendor/autoload.php
 
-versions=("3.1" "main")
 for version in "${versions[@]}"
 do
 	git clean -f
@@ -20,4 +22,14 @@ do
 	cp $root/pdg.config.yaml pdg.config.yaml
 	$root/tools/pdg guides $root/data/docs/guides/$version
 	$root/tools/pdg references $root/core.temp/src $root/data/docs/reference/$version --base-url /docs/v$version/reference
+done
+
+mkdir -p $root/data/docs/changelog
+for version in "${allVersions[@]}"
+do
+	git clean -f
+	git restore .
+	git fetch --depth=1 origin $version
+	git checkout FETCH_HEAD -b $version-temp
+	cp $root/core.temp/CHANGELOG.md $root/data/docs/changelog/$version.mdx
 done
