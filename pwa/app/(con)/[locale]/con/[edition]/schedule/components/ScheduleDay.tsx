6f51ac8118ce17610293439b296b95900137e5ef
ginifizz <laury@les-tilleuls.coop>
2024-09-17 15:14:04 +0200

    fix: remove outline on schedule anchors

--- pwa/app/(con)/[locale]/con/[edition]/schedule/components/ScheduleDay.tsx
+++ pwa/app/(con)/[locale]/con/[edition]/schedule/components/ScheduleDay.tsx
@@ -90,14 +90,18 @@ export default function ScheduleDay({
     return acc;
   }, [] as string[]);
 
-  const daySlug = slugify(day.title?.[locale] || '');
+  const daySlug = slugify(day.title?.[locale] || "");
 
   return day ? (
     <div className="mb-14 last:mb-0" key={day.title?.[locale]}>
-      <a id={daySlug} href={`#${daySlug}`} className='block -mt-24 pt-24'>
-      <h2 className="text-white font-title text-4xl font-bold">
-        {day.title?.[locale]}
-      </h2>
+      <a
+        id={daySlug}
+        href={`#${daySlug}`}
+        className="block -mt-24 pt-24 outline-none"
+      >
+        <h2 className="text-white font-title text-4xl font-bold">
+          {day.title?.[locale]}
+        </h2>
       </a>
       <Overline className="text-white/80 py-2">
         {" "}
