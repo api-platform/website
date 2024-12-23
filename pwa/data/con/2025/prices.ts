842abd9dc47145d61e1fb452561bdb21664b7b3b
ginifizz <laury@les-tilleuls.coop>
2024-12-24 00:56:35 +0100

    feat: add register page

---
+++ pwa/data/con/2025/prices.ts
@@ -0,0 +1,55 @@
+import { Price } from "types/con";
+
+const prices: Price[] = [
+  {
+    id: 1,
+    title: {
+      fr: "Conference",
+      en: "Conference",
+    },
+    languages: {
+      fr: "Talks en français et en anglais",
+      en: "French and english-speaking talks",
+    },
+    offers: [
+      {
+        type: "Early Bird",
+        title: {
+          fr: "Ticket 2 jours <br>(Early bird)</br>",
+          en: "2 days on-site event <br>(Early bird)</br>",
+        },
+        price: 139,
+        limitDate: "2025-05-13",
+      },
+      {
+        type: "Regular",
+        title: {
+          fr: "Ticket 2 jours <br>(Regular)</br>",
+          en: "2 days on-site event <br>(Regular)</br>",
+        },
+        price: 179,
+        startDate: "2025-05-14",
+        limitDate: "2025-07-29",
+      },
+      {
+        type: "Late bird",
+        title: {
+          fr: "Ticket 2 jours <br>(Late bird)</br>",
+          en: "2 days on-site event <br>(Late bird)</br>",
+        },
+        price: 239,
+        startDate: "2025-07-30",
+        limitDate: "2025-09-23",
+      },
+      {
+        title: {
+          fr: "Ticket online",
+          en: "Online ticket",
+        },
+        price: 105,
+      },
+    ],
+  },
+];
+
+export default prices;
